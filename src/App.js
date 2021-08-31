3c7d930ba003c09d684c857bde0173f7ca324b17
Vidisha Shah <shahvidisha1996@gmail.com>
2021-08-31 14:40:47 +0530

    Articles fetch by the voice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React,{useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
 
 
 const alanKey = '900a3555ab104bdc3bc9ed6228dcc7d52e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
 
+    const [newsArticles, setNewsArticles] = useState([]);
+
     useEffect(()=>{
         alanBtn({
             key:alanKey,
-            onCommand:({command})=>{
-                if(command==='testCommand')
+            onCommand:({command,articles})=>{
+                if(command==='newHeadLines')
                 {
-                    alert('This code executed');
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -23,6 +26,7 @@ const App = () =>{
             <h1>
                 Alan AI News Application
             </h1>
+            <NewsCards articles={newsArticles}/>
         </div>
     );
 }
